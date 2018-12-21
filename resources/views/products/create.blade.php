25a17644bce70efaffa5ded692d78906db6862b4
jcmlumacad1 <jcmlumacad@tmj.ph>
2018-12-21 16:59:32 +0800

    added view

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,14 @@
+<form>
+    <div class="form-group">
+        <label for="product-name">Name</label>
+        <input type="text" class="form-control" id="product-name">
+    </div>
+    <div class="form-group">
+        <label for="product-description">Description</label>
+        <input type="text" class="form-control" id="product-description">
+    </div>
+    <div class="form-group">
+        <label for="product-price">Price</label>
+        <input type="number" class="form-control" id="product-price">
+    </div>
+</form>
