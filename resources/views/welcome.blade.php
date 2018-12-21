e13ec54cc1716b664569878ed03f45c02eace945
jcmlumacad1 <jcmlumacad@tmj.ph>
2018-12-21 10:10:06 +0800

    edited welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>E-commerce App</title>
     </head>
     <body>
+        <div id="root"></div>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
